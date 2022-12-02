13e3a30bdb5e34f11558c487533f44d2072595d4
Xingchao Liu <xcliu@cs.utexas.edu>
2022-12-02 06:58:09 -0600

    Image generation first update

---
+++ ImageGeneration/main.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Training and evaluation"""
+
+import run_lib
+import run_lib_get_gmm
+import run_lib_rematch
+import run_lib_pytorch
+import run_lib_rematch_pytorch
+import run_lib_inversion
+from absl import app
+from absl import flags
+from ml_collections.config_flags import config_flags
+import logging
+import os
+import tensorflow as tf
+
+FLAGS = flags.FLAGS
+
+config_flags.DEFINE_config_file(
+  "config", None, "Training configuration.", lock_config=True)
+flags.DEFINE_string("workdir", None, "Work directory.")
+flags.DEFINE_enum("mode", None, ["train", "eval", "get_gmm", "rematch", "rematch-pytorch", "inversion"], "Running mode: train or eval")
+flags.DEFINE_string("eval_folder", "eval",
+                    "The folder name for storing evaluation results")
+flags.mark_flags_as_required(["workdir", "config", "mode"])
+
+
+def main(argv):
+  if FLAGS.mode == "train":
+    # Create the working directory
+    tf.io.gfile.makedirs(FLAGS.workdir)
+    # Set logger so that it outputs to both console and file
+    # Make logging work for both disk and Google Cloud Storage
+    gfile_stream = open(os.path.join(FLAGS.workdir, 'stdout.txt'), 'w')
+    handler = logging.StreamHandler(gfile_stream)
+    formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel('INFO')
+    # Run the training pipeline
+    if 'pytorch' in FLAGS.config.data.dataset.lower():
+        run_lib_pytorch.train(FLAGS.config, FLAGS.workdir)
+    else:
+        run_lib.train(FLAGS.config, FLAGS.workdir)
+  elif FLAGS.mode == "eval":
+    # Run the evaluation pipeline
+    if 'pytorch' in FLAGS.config.data.dataset.lower():
+        run_lib_pytorch.evaluate(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder)
+    else:
+        run_lib.evaluate(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder)
+  elif  FLAGS.mode == "reflow-pytorch":
+    run_lib_rematch_pytorch.train(FLAGS.config, FLAGS.workdir) 
+  elif  FLAGS.mode == "reflow":
+    run_lib_rematch.finetune_rematching(FLAGS.config, FLAGS.workdir)
+  else:
+    raise ValueError(f"Mode {FLAGS.mode} not recognized.")
+
+
+if __name__ == "__main__":
+  app.run(main)
