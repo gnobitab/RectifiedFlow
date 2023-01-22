1c2876bfd14df4dabb7808358a5000d818bbc69e
Xingchao Liu <xcliu@cs.utexas.edu>
2023-01-22 10:39:50 -0600

    update losses

--- ImageGeneration/losses.py
+++ ImageGeneration/losses.py
@@ -20,7 +20,7 @@ import torch
 import torch.optim as optim
 import numpy as np
 from models import utils as mutils
-from sde_lib import VESDE, VPSDE, RectifiedFlow
+from sde_lib import RectifiedFlow
 
 
 def get_optimizer(config, params):
@@ -52,109 +52,26 @@ def optimization_manager(config):
   return optimize_fn
 
 
-def get_sde_loss_fn(sde, train, reduce_mean=True, continuous=True, likelihood_weighting=True, eps=1e-5):
-  """Create a loss function for training with arbirary SDEs.
+def get_rectified_flow_loss_fn(sde, train, reduce_mean=True, eps=1e-3):
+  """Create a loss function for training with rectified flow.
 
   Args:
     sde: An `sde_lib.SDE` object that represents the forward SDE.
     train: `True` for training loss and `False` for evaluation loss.
     reduce_mean: If `True`, average the loss across data dimensions. Otherwise sum the loss across data dimensions.
-    continuous: `True` indicates that the model is defined to take continuous time steps. Otherwise it requires
-      ad-hoc interpolation to take continuous time steps.
-    likelihood_weighting: If `True`, weight the mixture of score matching losses
-      according to https://arxiv.org/abs/2101.09258; otherwise use the weighting recommended in our paper.
     eps: A `float` number. The smallest time step to sample from.
 
   Returns:
     A loss function.
   """
+
   reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
 
   def loss_fn(model, batch):
     """Compute the loss function.
 
     Args:
-      model: A score model.
-      batch: A mini-batch of training data.
-
-    Returns:
-      loss: A scalar that represents the average loss value across the mini-batch.
-    """
-    score_fn = mutils.get_score_fn(sde, model, train=train, continuous=continuous)
-    t = torch.rand(batch.shape[0], device=batch.device) * (sde.T - eps) + eps
-    z = torch.randn_like(batch)
-    mean, std = sde.marginal_prob(batch, t)
-    perturbed_data = mean + std[:, None, None, None] * z
-    score = score_fn(perturbed_data, t)
-
-    if not likelihood_weighting:
-      losses = torch.square(score * std[:, None, None, None] + z)
-      losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
-    else:
-      g2 = sde.sde(torch.zeros_like(batch), t)[1] ** 2
-      losses = torch.square(score + z / std[:, None, None, None])
-      losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1) * g2
-
-    loss = torch.mean(losses)
-    return loss
-
-  return loss_fn
-
-
-def get_smld_loss_fn(vesde, train, reduce_mean=False):
-  """Legacy code to reproduce previous results on SMLD(NCSN). Not recommended for new work."""
-  assert isinstance(vesde, VESDE), "SMLD training only works for VESDEs."
-
-  # Previous SMLD models assume descending sigmas
-  smld_sigma_array = torch.flip(vesde.discrete_sigmas, dims=(0,))
-  reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
-
-  def loss_fn(model, batch):
-    model_fn = mutils.get_model_fn(model, train=train)
-    labels = torch.randint(0, vesde.N, (batch.shape[0],), device=batch.device)
-    sigmas = smld_sigma_array.to(batch.device)[labels]
-    noise = torch.randn_like(batch) * sigmas[:, None, None, None]
-    perturbed_data = noise + batch
-    score = model_fn(perturbed_data, labels)
-    target = -noise / (sigmas ** 2)[:, None, None, None]
-    losses = torch.square(score - target)
-    losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1) * sigmas ** 2
-    loss = torch.mean(losses)
-    return loss
-
-  return loss_fn
-
-
-def get_ddpm_loss_fn(vpsde, train, reduce_mean=True):
-  """Legacy code to reproduce previous results on DDPM. Not recommended for new work."""
-  assert isinstance(vpsde, VPSDE), "DDPM training only works for VPSDEs."
-
-  reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
-
-  def loss_fn(model, batch):
-    model_fn = mutils.get_model_fn(model, train=train)
-    labels = torch.randint(0, vpsde.N, (batch.shape[0],), device=batch.device)
-    sqrt_alphas_cumprod = vpsde.sqrt_alphas_cumprod.to(batch.device)
-    sqrt_1m_alphas_cumprod = vpsde.sqrt_1m_alphas_cumprod.to(batch.device)
-    noise = torch.randn_like(batch)
-    perturbed_data = sqrt_alphas_cumprod[labels, None, None, None] * batch + \
-                     sqrt_1m_alphas_cumprod[labels, None, None, None] * noise
-    score = model_fn(perturbed_data, labels)
-    losses = torch.square(score - noise)
-    losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
-    loss = torch.mean(losses)
-    return loss
-
-  return loss_fn
-
-def get_rectified_flow_loss_fn(sde, train, reduce_mean=True, eps=1e-3):
-  reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
-
-  def loss_fn(model, batch):
-    """Compute the loss function.
-
-    Args:
-      model: A score model.
+      model: A velocity model.
       batch: A mini-batch of training data.
 
     Returns:
@@ -236,11 +153,7 @@ def get_step_fn(sde, train, optimize_fn=None, reduce_mean=False, continuous=True
                               continuous=True, likelihood_weighting=likelihood_weighting)
   else:
     assert not likelihood_weighting, "Likelihood weighting is not supported for original SMLD/DDPM training."
-    if isinstance(sde, VESDE):
-      loss_fn = get_smld_loss_fn(sde, train, reduce_mean=reduce_mean)
-    elif isinstance(sde, VPSDE):
-      loss_fn = get_ddpm_loss_fn(sde, train, reduce_mean=reduce_mean)
-    elif isinstance(sde, RectifiedFlow):
+    if isinstance(sde, RectifiedFlow):
       loss_fn = get_rectified_flow_loss_fn(sde, train, reduce_mean=reduce_mean)
     else:
       raise ValueError(f"Discrete training for {sde.__class__.__name__} is not recommended.")
